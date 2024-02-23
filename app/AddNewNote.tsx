7a46149bccabe7eb41bcaa75c91bf2f1495908cf
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-02-23 20:44:08 +0300

    move Modal to a new component

--- app/AddNewNote.tsx
+++ app/AddNewNote.tsx
@@ -11,31 +11,14 @@ import {
   Input,
 } from "@nextui-org/react";
 import "./globals.css";
-import SimpleMDE from "react-simplemde-editor";
+import dynamic from "next/dynamic";
+
+const Editor = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
 
 const AddNewNote = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
-
-  const options = {
-    toolbar: [
-      "bold",
-      "italic",
-      "heading",
-      "|",
-      "unordered-list",
-      "ordered-list",
-      "|",
-      "code",
-      "quote",
-      "link",
-      "|",
-      "undo",
-      "redo",
-      "|",
-      "preview",
-    ] as any,
-    scrollbarStyle: "native",
-  };
 
   return (
     <div>
@@ -45,62 +28,73 @@ const AddNewNote = () => {
             isClearable
             readOnly={true}
             radius="lg"
-            classNames={{
-              label: "text-black/50 dark:text-white/90",
-              input: [
-                "bg-transparent",
-                "text-black/90 dark:text-white/90",
-                "placeholder:text-default-700/50 dark:placeholder:text-white/60",
-              ],
-              innerWrapper: "bg-transparent",
-              inputWrapper: [
-                "shadow-xl",
-                "bg-default-200/50",
-                "dark:bg-default/60",
-                "backdrop-blur-xl",
-                "backdrop-saturate-200",
-                "hover:bg-default-200/70",
-                "dark:hover:bg-default/70",
-                "group-data-[focused=true]:bg-default-200/50",
-                "dark:group-data-[focused=true]:bg-default/60",
-                "!cursor-text",
-              ],
-            }}
             placeholder="New Note..."
           />
         </div>
+        <CreateNewNote isOpen={isOpen} onClose={onClose} />
       </div>
-
-      <Modal
-        backdrop="blur"
-        isOpen={isOpen}
-        onClose={onClose}
-        size="5xl"
-        scrollBehavior="inside"
-        disableAnimation={true}
-      >
-        <ModalContent className="p-3 w-full">
-          <ModalHeader className="flex flex-col gap-1">
-            <Input type="text" label="Title" size="sm" />
-          </ModalHeader>
-          <ModalBody
-            className="w-full overflow-auto"
-            style={{ maxHeight: "500px" }}
-          >
-            <SimpleMDE placeholder="Note" options={options} autoSave="true" />
-          </ModalBody>
-          <ModalFooter>
-            <Button color="danger" variant="light" onPress={onClose}>
-              Close
-            </Button>
-            <Button color="primary" onPress={onClose}>
-              Create
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
     </div>
   );
+};
+
+const CreateNewNote = ({
+  isOpen,
+  onClose,
+}: {
+  isOpen: boolean;
+  onClose: () => void;
+}) => {
+  return (
+    <Modal
+      backdrop="blur"
+      isOpen={isOpen}
+      onClose={onClose}
+      size="5xl"
+      scrollBehavior="inside"
+      disableAnimation={true}
+    >
+      <ModalContent className="p-3 w-full">
+        <ModalHeader className="flex flex-col gap-1">
+          <Input type="text" label="Title" size="sm" />
+        </ModalHeader>
+        <ModalBody
+          className="w-full overflow-auto"
+          style={{ maxHeight: "500px" }}
+        >
+          <Editor placeholder="Note" options={options} autoSave="true" />
+        </ModalBody>
+        <ModalFooter>
+          <Button color="danger" variant="light" onPress={onClose}>
+            Close
+          </Button>
+          <Button color="primary" onPress={onClose}>
+            Create
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+const options = {
+  toolbar: [
+    "bold",
+    "italic",
+    "heading",
+    "|",
+    "unordered-list",
+    "ordered-list",
+    "|",
+    "code",
+    "quote",
+    "link",
+    "|",
+    "undo",
+    "redo",
+    "|",
+    "preview",
+  ] as any,
+  scrollbarStyle: "native",
 };
 
 export default AddNewNote;
