172c852111cbc03567ff76c88060c05ab3466b67
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-02-28 00:44:10 +0300

    remove old hook

--- app/hooks/useGetNotes.ts
+++ app/hooks/useGetNotes.ts
@@ -1,18 +1,5 @@
 import { Note } from "@prisma/client";
-import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
-
-export const useGetNotes = () => {
-  return useQuery<Note[], Error>({
-    queryKey: ["notes"],
-    queryFn: () =>
-      fetch("/api/note/list", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }).then((value) => value.json()),
-  });
-};
+import { useInfiniteQuery } from "@tanstack/react-query";
 
 interface ApiResponse {
   data: Note[];
