da580607b2df6adbd35af4b1c72d8b1b414a12af
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-02-28 16:11:31 +0300

    switch to server component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,6 @@
-"use client";
 import NavigationBar from "./NavigationBar";
 import AddNewNote from "./AddNewNote";
 import Notes from "./Notes";
-import { useGetInfiniteNotes } from "./hooks/useGetNotes";
-import { useEffect } from "react";
-import { useNotesContext } from "./hooks/useNotesContext";
 
 export default function Home() {
   return (
