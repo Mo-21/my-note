c7331703d4ecf7e58482fabab9e1b973ce8c4e63
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-02-29 14:57:58 +0300

    add react query devtools

--- app/providers/ReactQueryProvider.tsx
+++ app/providers/ReactQueryProvider.tsx
@@ -1,11 +1,15 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools/production";
 import React, { PropsWithChildren } from "react";
 
 const ReactQueryProvider = ({ children }: PropsWithChildren) => {
   const queryClient = new QueryClient();
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 };
 
