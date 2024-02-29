5c39a24953ff350d9d33141e8697495467206061
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-03-01 02:19:55 +0300

    add a PATCH request to edit a note

---
+++ app/api/note/edit/route.ts
@@ -0,0 +1,30 @@
+import { getServerSession } from "next-auth/next";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PATCH(req: NextRequest) {
+  const session = await getServerSession();
+
+  if (!session || !session.user?.email)
+    return NextResponse.json({ message: "Unauthorized" }, { status: 403 });
+
+  const body: {
+    id: number;
+    content: string;
+    title?: string | null | undefined;
+  } = await req.json();
+
+  if (!body)
+    return NextResponse.json({ message: "No ID provided" }, { status: 403 });
+
+  const updatedNote = await prisma?.note.update({
+    where: {
+      id: body.id,
+    },
+    data: {
+      title: body.title,
+      content: body.content,
+    },
+  });
+
+  return NextResponse.json(updatedNote, { status: 201 });
+}
