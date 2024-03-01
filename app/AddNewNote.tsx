0c92d0857e90db06147c235fbeae740d33097050
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-03-01 16:40:33 +0300

    add buttons for quick note and checkbox

--- app/AddNewNote.tsx
+++ app/AddNewNote.tsx
@@ -1,6 +1,6 @@
 "use client";
 import "easymde/dist/easymde.min.css";
-import { useDisclosure, Input } from "@nextui-org/react";
+import { useDisclosure, Button, ButtonGroup } from "@nextui-org/react";
 import "./globals.css";
 import NewNoteForm from "./components/NewNoteForm";
 
@@ -9,21 +9,38 @@ const AddNewNote = () => {
 
   return (
     <div>
-      <div className="flex justify-center px-4 mt-3">
-        <div
-          onClick={() => onOpen()}
-          className="max-w-[740px] w-full h-max rounded-2xl flex justify-center items-center text-white shadow-sm backdrop-blur dark:bg-black/30"
-        >
-          <Input
-            isClearable
-            readOnly={true}
-            radius="lg"
-            placeholder="New Note..."
-          />
-        </div>
+      <div className="flex justify-evenly items-center px-4 mt-3">
+        <CreateNoteButtons onOpen={onOpen} />
         <NewNoteForm isOpen={isOpen} onClose={onClose} isUpdating={false} />
       </div>
     </div>
+  );
+};
+
+const CreateNoteButtons = ({ onOpen }: { onOpen: () => void }) => {
+  const buttons: { key: string; value: string; fn?: () => void }[] = [
+    {
+      key: "quick_note",
+      value: "Quick Note",
+    },
+    {
+      key: "quick_note",
+      value: "Editor",
+      fn: () => onOpen(),
+    },
+    {
+      key: "quick_note",
+      value: "Checkbox",
+    },
+  ];
+  return (
+    <ButtonGroup variant="ghost" className="flex">
+      {buttons.map((b) => (
+        <Button onClick={b.fn} key={b.key}>
+          {b.value}
+        </Button>
+      ))}
+    </ButtonGroup>
   );
 };
 
