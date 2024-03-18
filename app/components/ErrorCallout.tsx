0a7e1f20754b71a7af0cdd438001743f4660128b
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-03-18 17:52:37 +0300

    add margin

--- app/components/ErrorCallout.tsx
+++ app/components/ErrorCallout.tsx
@@ -5,7 +5,7 @@ import { PropsWithChildren } from "react";
 const ErrorCallout = ({ children }: PropsWithChildren) => {
   if (!children) return null;
 
-  return <div className="text-xs text-red-500">{children}</div>;
+  return <div className="text-xs text-red-500 mt-2">{children}</div>;
 };
 
 export default ErrorCallout;
